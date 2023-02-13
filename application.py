8a3521c52111a8244a518832c16bce32eecd4050
yyamaguchi10 <124410186+yyamaguchi10@users.noreply.github.com>
2023-02-13 22:41:10 +0900

    r20230213v2

--- application.py
+++ application.py
@@ -123,9 +123,10 @@ def index():
     #min_prices = data[data['today_prices'] == data['today_prices'].min()]
     text_min = str(round(min_price,2))
 
-    style = today_prices.style.highlight_min(color="yellow")
-    style = today_prices.style.highlight_max(color="red")
-
+    #style = today_prices.style.highlight_min(color="yellow")
+    #style = today_prices.style.highlight_max(color="red")
+    today_prices.insert(0,'time', ["0:00-","0:30-","1:00-","1:30-","2:00-","2:30-","3:00-","3:30-","4:00-","4:30-","5:00-","5:30-","6:00-","6:30-","7:00-","7:30-","8:00-","8:30-","9:00-","9:30-","10:00-","10:30-","11:00-","11:30-","12:00-","12:30-","13:00-","13:30-","14:00-","14:30-","15:00-","15:30-","16:00-","16:30-","17:00-","17:30-","18:00-","18:30-","19:00-","19:30-","20:00-","20:30-","21:00-","21:30-","22:00-","22:30-","23:00-","23:30-"])
+    today_prices = today_prices.set_index('time',inplace=True)
 
     # グラフをテンプレートに渡す
     return render_template('index.html', img=img,text_max=text_max,text_min=text_min,today_prices=today_prices.to_html(classes='data', header="true"))
